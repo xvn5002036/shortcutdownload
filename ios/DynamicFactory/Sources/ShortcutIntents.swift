4e3fe470398ad59dd8cc076ebfda036587741701
XVN5002036 <89866299+xvn5002036@users.noreply.github.com>
2026-08-23 18:27:20 +0800

    新增捷徑直接生成 Live Photo 的動圖工廠動作

--- ios/DynamicFactory/Sources/ShortcutIntents.swift
+++ ios/DynamicFactory/Sources/ShortcutIntents.swift
@@ -1,8 +1,20 @@
 import AppIntents
+import UniformTypeIdentifiers
 
 struct DynamicFactoryShortcuts: AppShortcutsProvider {
     static var appShortcuts: [AppShortcut] {
-        AppShortcut(intent: OpenDynamicFactoryIntent(), phrases: ["開啟 \(.applicationName)", "使用 \(.applicationName)"], shortTitle: "開啟動圖工廠", systemImageName: "photo.on.rectangle")
+        AppShortcut(
+            intent: OpenDynamicFactoryIntent(),
+            phrases: ["開啟 \(.applicationName)", "使用 \(.applicationName)"],
+            shortTitle: "開啟動圖工廠",
+            systemImageName: "photo.on.rectangle"
+        )
+        AppShortcut(
+            intent: MakeLivePhotoIntent(),
+            phrases: ["用 \(.applicationName) 生成 Live Photo", "使用 \(.applicationName) 製作實況照片"],
+            shortTitle: "生成 Live Photo",
+            systemImageName: "livephoto"
+        )
     }
 }
 
@@ -13,5 +25,30 @@ struct OpenDynamicFactoryIntent: AppIntent {
 
     func perform() async throws -> some IntentResult {
         .result()
+    }
+}
+
+struct MakeLivePhotoIntent: AppIntent {
+    static var title: LocalizedStringResource = "生成 Live Photo"
+    static var description = IntentDescription("接收一張圖片與一段影片，直接生成 Live Photo 並存入照片圖庫。")
+    static var openAppWhenRun = false
+
+    @Parameter(title: "圖片", supportedTypeIdentifiers: [UTType.image.identifier])
+    var photo: IntentFile
+
+    @Parameter(title: "影片", supportedTypeIdentifiers: [UTType.movie.identifier, UTType.video.identifier])
+    var video: IntentFile
+
+    static var parameterSummary: some ParameterSummary {
+        Summary("用 \(.$photo) 和 \(.$video) 生成 Live Photo")
+    }
+
+    func perform() async throws -> some IntentResult & ProvidesDialog {
+        let result = await ShortcutMediaBridge.shared.makeLivePhoto(
+            photoData: photo.data,
+            videoData: video.data,
+            videoFilename: video.filename
+        )
+        return .result(dialog: IntentDialog(stringLiteral: result))
     }
 }
